f2a3503147ec99e653a5a9cf4e093744fcdd7e58
khoa <hatgiong@gmail.com>
2022-01-19 05:51:19 +0700

    dùng dạng quey

--- app/src/main/java/com/example/retrofit1810/ApiRequest.java
+++ app/src/main/java/com/example/retrofit1810/ApiRequest.java
@@ -1,4 +1,14 @@
 package com.example.retrofit1810;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiRequest {
+    //?appid=57f262786122665cbe313bc17c68c604&units=metric&q=Hanoi
+    //dùng dạng Query
+    @GET("data/2.5/weather")
+    Call<String> getTempCity(@Query("appid")String appid,@Query("units")String units,@Query("q")String q);
+
 }
+
